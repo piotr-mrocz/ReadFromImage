fe190b33b90af3dd2023df19551cb48d0a6c2333
Piotr Mroczkowski <p.mrocz@vp.pl>
2023-04-22 21:01:07 +0200

    Read tet from image

---
+++ ReadTetFromImage/ReadTetFromImage/Program.cs
@@ -0,0 +1,14 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+using Patagames.Ocr;
+
+Console.WriteLine("Hello, World!");
+
+var image = @"D:\Nauka\Repos\ReadTetFromImage\ReadTetFromImage\photo.png";
+
+using (var objOcr = OcrApi.Create())
+{
+    objOcr.Init(Patagames.Ocr.Enums.Languages.English);
+    var text = objOcr.GetTextFromImage(image);
+    Console.WriteLine(text);
+}
